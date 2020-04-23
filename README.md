6ac2efd510e7be862a9100c8b341690241cf51d8
badc0re <dame.jovanoski@gmail.com>
2020-04-23 09:50:36 +0200

    Added samples.

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# Hell programming language
+# Hel programming language
 This is a functional language.
+
+Philosophy
+- Code should be readable
+- No states, only transformations
+- Everything is a function except variables
+- Variables are immutable
+- Functions can be referenced by aliases
+
 Lexer (todo):
 - Change handling of error messages, instead of bools, use a wrapper for the errors
 - Parsing numbers
--- core/parser.go
+++ core/parser.go
@@ -4,3 +4,136 @@ type Parser struct {
 	tokens  []Token
 	current int
 }
+
+func (parser Parser) peek() Token {
+	return parser.tokens[parser.current]
+}
+
+func (parser Parser) check(ttype TokenType) bool {
+	if ttype == EOF {
+		return false
+	}
+	return parser.peek().tokenType == ttype
+}
+
+func (parser Parser) advance() Token {
+	if !parser.isEOF() {
+		parser.current++
+	}
+	return parser.previous()
+}
+
+func (parser Parser) isEOF() bool {
+	return parser.peek().tokenType == EOF
+}
+
+func (parser Parser) previous() Token {
+	return parser.tokens[parser.current-1]
+}
+
+
+func (parser Parser) match(types ...TokenType) bool {
+	for _, ttype := range types {
+		if parser.check(ttype) {
+			parser.advance()
+			return true
+		}
+	}
+	return false
+}
+
+func (parser Parser) primary() Expr {
+	if parser.match(FALSE)
+	return Literal{value:"false"} 
+	}
+
+	if parser.match(TRUE) {
+		return Literal{"true"} 
+	}
+
+	if parser.match(NIL) {
+		return Literal{"nil"} 
+	}
+
+	if parser.match(NUMBER, STRING) {
+		return Literal(previous().literal)
+	}
+	
+	if parser.match(OP) {
+
+	}
+}
+
+func (parser Parser) unary() Expr {
+	if parser.match(EXCL, MINUS) {
+		operator := parser.previous()
+		right := parser.unary()
+		return Unary{operator, right}
+	}
+	return primary()
+}
+
+func (parser Parser) multiplication() Expr {
+	expr := unary()
+
+	for match(SLASH, STAR) {
+		operator := previous()
+		right := unary()
+		expr = Binary{
+			operator: operator,
+			left:     expr,
+			right:    right,
+		}
+	}
+	return expr
+}
+
+func (parser Parser) addition() Expr {
+	expr := multiplication()
+
+	for match(MINUS, PLUS) {
+		operator := previous()
+		right := multiplication()
+		expr = Binary{
+			operator: operator,
+			left:     expr,
+			right:    right,
+		}
+	}
+	return expr
+}
+
+func (parser Parser) comparison() Expr {
+	expr := addition()
+
+	for match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL) {
+		operator := previous()
+		right := addition()
+		expr = Binary{
+			operator: operator,
+			left:     expr,
+			right:    right,
+		}
+	}
+	return expr
+}
+
+func (parser Parser) equality() Expr {
+	// (or (== (/ 1 2) 2) (== 33 44))
+	expr := comparison()
+
+	for match(EXCL_EQUAL, EQUAL_EQUAL) {
+		operator := previous()
+		right := comparison()
+		expr = Binary{
+			operator: operator,
+			left:     expr,
+			right:    right,
+		}
+	}
+	return expr
+}
+
+func (parser Parser) expression() Expr {
+	return equality()
+}
